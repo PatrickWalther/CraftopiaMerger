99527fb55c84f9c53167c905d6af75835b5b603d
Patrick Walther <develop@pwalther.ch>
2020-09-12 17:20:13 +0200

    Initial commit

---
+++ CraftopiaMerger/Helpers/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CraftopiaMerger.Helpers
+{
+    public static class StringExtensions
+    {
+        public static MemoryStream ToMemoryStream(this string str)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
